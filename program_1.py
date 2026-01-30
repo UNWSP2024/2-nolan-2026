ec449b2f8ab7283ce7e6fe1b7c63e360aa960414
nolan-2026 <nrnelsen@students.unwsp.edu>
2026-01-30 14:35:39 -0600

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -3,6 +3,10 @@ def personal_information():
     address = "5029 Cherry Road, Springfield, MI, 59309"
     phone_number = "943-295-1958"
     major = "Culinary Arts"
+    print(name)
+    print(address)
+    print(phone_number)
+    print(major)
 
 # Line which calls the above function.
 personal_information()
