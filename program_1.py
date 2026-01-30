c66bc0bc6e4a0df0f67bc674ad70fbaa2b7d7595
nolan-2026 <nrnelsen@students.unwsp.edu>
2026-01-30 14:26:38 -0600

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -2,5 +2,5 @@ def personal_information():
 	print('Tanka Jahari, 2509 Cherry Drive, Springfield, MI, 23095, 950-385-6579, Culinary Arts Major')
 
 
-# Line which calls the above function.
+	# Line which calls the above function.
 personal_information()
