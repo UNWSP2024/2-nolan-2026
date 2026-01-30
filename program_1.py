ac28cbed59c673429d99aa26aee958b77cec8553
nolan-2026 <nrnelsen@students.unwsp.edu>
2026-01-30 14:31:50 -0600

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,13 +1,12 @@
 def personal_information():
-	name = "Tanka Jahari"
-	address = "5029 Cherry Road, Springfield, MI, 59309"
-	phone_number = "943-295-1958"
-	major = "Culinary Arts"
 	print(name)
+	name = "Tanka Jahari"
 	print(address)
+	address = "5029 Cherry Road, Springfield, MI, 59309"
 	print(phone_number)
+	phone_number = "943-295-1958"
 	print(major)
-
+	major = "Culinary Arts"
 
 # Line which calls the above function.
 personal_information()
